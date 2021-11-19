44af093ddc80272521543afe0c0fb5870d42486f
Vidit Jain <viditjain@live.com>
2021-11-19 12:40:08 +0530

    clang-format, did some cleaning

--- q2/client.cpp
+++ q2/client.cpp
@@ -1,27 +1,26 @@
+#include <arpa/inet.h>
+#include <bits/stdc++.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
-#include <netinet/in.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <string.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <bits/stdc++.h>
 /////////////////////////////
-#include <pthread.h>
-#include <iostream>
-#include <semaphore.h>
 #include <assert.h>
+#include <iostream>
+#include <pthread.h>
 #include <queue>
-#include <vector>
+#include <semaphore.h>
 #include <tuple>
+#include <vector>
 using namespace std;
 /////////////////////////////
 
-//Regular bold text
+// Regular bold text
 #define BBLK "\e[1;30m"
 #define BRED "\e[1;31m"
 #define BGRN "\e[1;32m"
@@ -31,159 +30,107 @@ using namespace std;
 #define BCYN "\e[1;36m"
 #define ANSI_RESET "\x1b[0m"
 
-typedef long long LL;
-const LL MOD = 1000000007;
-#define part cout << "-----------------------------------" << endl;
-#define pb push_back
-#define debug(x) cout << #x << " : " << x << endl
 
-///////////////////////////////
 #define SERVER_PORT 8001
-////////////////////////////////////
 
-const LL buff_sz = 1048576;
-///////////////////////////////////////////////////
-pair<string, int> read_string_from_socket(int fd, int bytes)
-{
-    std::string output;
-    output.resize(bytes);
+const int buff_sz = 1048576;
+string read_string_from_socket(int fd) {
+	std::string output;
+	output.resize(buff_sz);
 
-    int bytes_received = read(fd, &output[0], bytes - 1);
-    // debug(bytes_received);
-    if (bytes_received <= 0)
-    {
-        cerr << "Failed to read data from socket. Seems server has closed socket\n";
-        // return "
-        exit(-1);
-    }
+	int bytes_received = read(fd, &output[0], buff_sz- 1);
+	if (bytes_received <= 0) {
+		cerr << "Failed to read data from socket. Seems server has closed "
+				"socket\n";
+		exit(-1);
+	}
 
-    // debug(output);
-    output[bytes_received] = 0;
-    output.resize(bytes_received);
+	output[bytes_received] = 0;
+	output.resize(bytes_received);
 
-    return {output, bytes_received};
+    return output;
 }
 
-int send_string_on_socket(int fd, const string &s)
-{
-    // cout << "We are sending " << s << endl;
-    int bytes_sent = write(fd, s.c_str(), s.length());
-    // debug(bytes_sent);
-    // debug(s);
-    if (bytes_sent < 0)
-    {
-        cerr << "Failed to SEND DATA on socket.\n";
-        // return "
-        exit(-1);
-    }
+int send_string_on_socket(int fd, const string &s) {
+	int bytes_sent = write(fd, s.c_str(), s.length());
+	if (bytes_sent < 0) {
+		cerr << "Failed to SEND DATA on socket.\n";
+		exit(-1);
+	}
 
-    return bytes_sent;
+	return bytes_sent;
 }
 
-int get_socket_fd(struct sockaddr_in *ptr)
-{
-    struct sockaddr_in server_obj = *ptr;
+int get_socket_fd(struct sockaddr_in *ptr) {
+	struct sockaddr_in server_obj = *ptr;
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd < 0) {
+		perror("Error in socket creation for CLIENT");
+		exit(-1);
+	}
+	int port_num = SERVER_PORT;
 
-    // socket() creates an endpoint for communication and returns a file
-    //        descriptor that refers to that endpoint.  The file descriptor
-    //        returned by a successful call will be the lowest-numbered file
-    //        descriptor not currently open for the process.
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket_fd < 0)
-    {
-        perror("Error in socket creation for CLIENT");
-        exit(-1);
-    }
-    /////////////////////////////////////////////////////////////////////////////////////
-    int port_num = SERVER_PORT;
+	memset(&server_obj, 0, sizeof(server_obj)); // Zero out structure
+	server_obj.sin_family = AF_INET;
+	server_obj.sin_port = htons(port_num); // convert to big-endian order
 
-    memset(&server_obj, 0, sizeof(server_obj)); // Zero out structure
-    server_obj.sin_family = AF_INET;
-    server_obj.sin_port = htons(port_num); //convert to big-endian order
+	if (connect(socket_fd, (struct sockaddr *)&server_obj, sizeof(server_obj)) <
+		0) {
+		perror("Problem in connecting to the server");
+		exit(-1);
+	}
 
-    // Converts an IP address in numbers-and-dots notation into either a
-    // struct in_addr or a struct in6_addr depending on whether you specify AF_INET or AF_INET6.
-    //https://stackoverflow.com/a/20778887/6427607
-
-    /////////////////////////////////////////////////////////////////////////////////////////
-    /* connect to server */
-
-    if (connect(socket_fd, (struct sockaddr *)&server_obj, sizeof(server_obj)) < 0)
-    {
-        perror("Problem in connecting to the server");
-        exit(-1);
-    }
-
-    //part;
-    // printf(BGRN "Connected to server\n" ANSI_RESET);
-    // part;
-    return socket_fd;
+	return socket_fd;
 }
-////////////////////////////////////////////////////////
 struct abc {
-    int time;
-    char *x;
+	int time;
+	char *x;
 };
-void* begin_process(void* arg) {
-    struct abc s = *((struct abc*)arg);
-    // char message = s.x;
-    string message(s.x);
-    int time = s.time;
-    // char* message = s.x;
-    // cout << message << " QQ\n";
-    // cout << time << " QQ\n";
-    sleep(time);
-    struct sockaddr_in server_obj;
-    int socket_fd = get_socket_fd(&server_obj);
+void *begin_process(void *arg) {
+	struct abc s = *((struct abc *)arg);
+	string message(s.x);
+	int time = s.time;
+	sleep(time);
+	struct sockaddr_in server_obj;
+	int socket_fd = get_socket_fd(&server_obj);
 
+	cout << "Connection to server successful" << endl;
 
-    cout << "Connection to server successful" << endl;
-    // int requests; 
-    // cin >> requests;
-    // scanf("\n");
-    // string full_message = to_string(requests) + "\n";
-    // while (requests--) {
-    //     string to_send;
-    //     getline(cin, to_send);
-    //     to_send += "\n";
-    //     full_message += to_send;
-    // }
-    send_string_on_socket(socket_fd, message);
-    int num_bytes_read;
-    string output_msg;
-    tie(output_msg, num_bytes_read) = read_string_from_socket(socket_fd, buff_sz);
-    cout << output_msg;
-    return NULL;
+	send_string_on_socket(socket_fd, message);
+	string output_msg;
+    output_msg = read_string_from_socket(socket_fd);
+
+	cout << output_msg;
+	return NULL;
 }
-int main(int argc, char *argv[])
-{
-    int i, j, k, t, n;
-    int last_request = 0;
-    int request_count;
-    int fd;
-    cin >> request_count;
-    pthread_t user_requests[request_count];
-    vector<pair<int, string>> requests;
-    for (int i = 0; i < request_count; i++) {
-        string command;
-        int t;
-        cin >> t;
-        getline(cin, command);
-        requests.push_back({t, command});
-    }
-    for (int i = 0; i < request_count; i++) {
-        string x = to_string(i) + requests[i].second;
-        struct abc *container = (struct abc*) malloc(sizeof(struct abc));
-        char* q = (char*) x.c_str();
-        // cout << q << " YO YO JES\n";
-        container->x = (char*) malloc((strlen(q) + 1) * sizeof (char)); 
-        strcpy(container->x, q);
-        container->time = requests[i].first;
-        pthread_create(&user_requests[i], NULL, begin_process, (void *) container);
-    }
+int main(int argc, char *argv[]) {
 
-    for (int i = 0; i < request_count; i++) {
-        pthread_join(user_requests[i], NULL);
-    }
-    return 0;
+	int request_count;
+	int fd;
+	cin >> request_count;
+	pthread_t user_requests[request_count];
+	vector<pair<int, string>> requests;
+	for (int i = 0; i < request_count; i++) {
+		string command;
+		int t;
+		cin >> t;
+		getline(cin, command);
+		requests.push_back({t, command});
+	}
+	for (int i = 0; i < request_count; i++) {
+		string x = to_string(i) + requests[i].second;
+		struct abc *container = (struct abc *)malloc(sizeof(struct abc));
+		char *q = (char *)x.c_str();
+
+		container->x = (char *)malloc((strlen(q) + 1) * sizeof(char));
+		strcpy(container->x, q);
+		container->time = requests[i].first;
+		pthread_create(&user_requests[i], NULL, begin_process,
+					   (void *)container);
+	}
+
+	for (int i = 0; i < request_count; i++) {
+		pthread_join(user_requests[i], NULL);
+	}
+	return 0;
 }
--- q2/server_prog.cpp
+++ q2/server_prog.cpp
@@ -1,24 +1,23 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include <netinet/in.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 /////////////////////////////
-#include <iostream>
 #include <assert.h>
-#include <tuple>
+#include <iostream>
 #include <queue>
+#include <tuple>
 using namespace std;
 /////////////////////////////
-#include<bits/stdc++.h>
-//Regular bold text
+#include <bits/stdc++.h>
+// Regular bold text
 #define BBLK "\e[1;30m"
 #define BRED "\e[1;31m"
 #define BGRN "\e[1;32m"
@@ -28,16 +27,12 @@ using namespace std;
 #define BCYN "\e[1;36m"
 #define ANSI_RESET "\x1b[0m"
 
-typedef long long LL;
 
-#define pb push_back
-#define debug(x) cout << #x << " : " << x << endl
-#define part cout << "-----------------------------------" << endl;
 
 #define MAX_CLIENTS 4
-#define PORT_ARG 8001
+#define SERVER_PORT 8001
 #define MAX_KEY 101
-const int initial_msg_len = 256;
+
 int pool_size;
 pthread_cond_t client_lock;
 pthread_t *thread_pool;
@@ -46,266 +41,245 @@ queue<int> clients;
 
 map<int, string> dictionary;
 pthread_mutex_t key_lock[MAX_KEY];
-const LL buff_sz = 1048576;
+const int buff_sz = 1048576;
 
-pair<string, int> read_string_from_socket(const int &fd, int bytes)
-{
-    std::string output;
-    output.resize(bytes);
+pair<string, int> read_string_from_socket(const int &fd, int bytes) {
+	std::string output;
+	output.resize(bytes);
 
-    int bytes_received = read(fd, &output[0], bytes - 1);
-    debug(bytes_received);
-    if (bytes_received <= 0)
-    {
-        cerr << "Failed to read data from socket. \n";
-    }
+	int bytes_received = read(fd, &output[0], bytes - 1);
+	if (bytes_received <= 0) {
+		cerr << "Failed to read data from socket. \n";
+	}
 
-    output[bytes_received] = 0;
-    output.resize(bytes_received);
-    // debug(output);
-    return {output, bytes_received};
+	output[bytes_received] = 0;
+	output.resize(bytes_received);
+	return {output, bytes_received};
 }
 
-int send_string_on_socket(int fd, const string &s)
-{
-    // debug(s.length());
-    int bytes_sent = write(fd, s.c_str(), s.length());
-    if (bytes_sent < 0)
-    {
-        cerr << "Failed to SEND DATA via socket.\n";
-    }
+int send_string_on_socket(int fd, const string &s) {
+	int bytes_sent = write(fd, s.c_str(), s.length());
+	if (bytes_sent < 0) {
+		cerr << "Failed to SEND DATA via socket.\n";
+	}
 
-    return bytes_sent;
+	return bytes_sent;
 }
 string insertDict(int key, string value) {
-    pthread_mutex_lock(&key_lock[key]);        
-    if (dictionary.find(key) == dictionary.end()) {
-        dictionary[key] = value;        
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "Insertion successful";
-    }
-    else {
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "Key already exists";
-    }
+	pthread_mutex_lock(&key_lock[key]);
+	if (dictionary.find(key) == dictionary.end()) {
+		dictionary[key] = value;
+		pthread_mutex_unlock(&key_lock[key]);
+		return "Insertion successful";
+	} else {
+		pthread_mutex_unlock(&key_lock[key]);
+		return "Key already exists";
+	}
 }
 string deleteDict(int key) {
-    pthread_mutex_lock(&key_lock[key]);        
-    if (dictionary.find(key) != dictionary.end()) {
-        dictionary.erase(key);
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "Deletion successful";
-    }
-    else {
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "No such key exists";
-    }
-
+	pthread_mutex_lock(&key_lock[key]);
+	if (dictionary.find(key) != dictionary.end()) {
+		dictionary.erase(key);
+		pthread_mutex_unlock(&key_lock[key]);
+		return "Deletion successful";
+	} else {
+		pthread_mutex_unlock(&key_lock[key]);
+		return "No such key exists";
+	}
 }
 string updateDict(int key, string value) {
-    pthread_mutex_lock(&key_lock[key]);        
-    if (dictionary.find(key) != dictionary.end()) {
-        dictionary[key] = value;        
-        pthread_mutex_unlock(&key_lock[key]);        
-        return value; 
-    }
-    else {
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "Key already exists";
-    }
-
+	pthread_mutex_lock(&key_lock[key]);
+	if (dictionary.find(key) != dictionary.end()) {
+		dictionary[key] = value;
+		pthread_mutex_unlock(&key_lock[key]);
+		return value;
+	} else {
+		pthread_mutex_unlock(&key_lock[key]);
+		return "Key already exists";
+	}
 }
 string concatDict(int key1, int key2) {
-    int lowKey = min(key1, key2), highKey = max(key1, key2);
-    pthread_mutex_lock(&key_lock[lowKey]);
-    pthread_mutex_lock(&key_lock[highKey]);
-    if (dictionary.find(key1) != dictionary.end() && dictionary.find(key2) != dictionary.end()) {
-        string val1 = dictionary[key1];
-        string val2 = dictionary[key2];
-        dictionary[key1] += val2;
-        dictionary[key2] += val1;
-        pthread_mutex_lock(&key_lock[highKey]);
-        pthread_mutex_lock(&key_lock[lowKey]);
-        return val2 + val1;
-    }
-    else {
-        pthread_mutex_lock(&key_lock[highKey]);
-        pthread_mutex_lock(&key_lock[lowKey]);
-        return "Concat failed as at least one of the keys does not exist";
-    }
+	int lowKey = min(key1, key2), highKey = max(key1, key2);
+	pthread_mutex_lock(&key_lock[lowKey]);
+	pthread_mutex_lock(&key_lock[highKey]);
+	if (dictionary.find(key1) != dictionary.end() &&
+		dictionary.find(key2) != dictionary.end()) {
+		string val1 = dictionary[key1];
+		string val2 = dictionary[key2];
+		dictionary[key1] += val2;
+		dictionary[key2] += val1;
+		pthread_mutex_lock(&key_lock[highKey]);
+		pthread_mutex_lock(&key_lock[lowKey]);
+		return val2 + val1;
+	} else {
+		pthread_mutex_lock(&key_lock[highKey]);
+		pthread_mutex_lock(&key_lock[lowKey]);
+		return "Concat failed as at least one of the keys does not exist";
+	}
 }
 string fetchDict(int key) {
-    pthread_mutex_lock(&key_lock[key]);        
-    if (dictionary.find(key) != dictionary.end()) {
-        string val = dictionary[key];
-        pthread_mutex_unlock(&key_lock[key]);        
-        return val;
-    }
-    else {
-        pthread_mutex_unlock(&key_lock[key]);        
-        return "Key does not exist";
-    }
-
+	pthread_mutex_lock(&key_lock[key]);
+	if (dictionary.find(key) != dictionary.end()) {
+		string val = dictionary[key];
+		pthread_mutex_unlock(&key_lock[key]);
+		return val;
+	} else {
+		pthread_mutex_unlock(&key_lock[key]);
+		return "Key does not exist";
+	}
 }
-///////////////////////////////
-string parseQueries(vector<string>& x) {
-    if (x[1] == "insert") {
-        int key;
-        string value;
-        key = atoi(x[2].c_str());
-        value = x[3];
-        return insertDict(key, value);
-    }
-    else if (x[1] == "delete") {
-        int key;
-        key = atoi(x[2].c_str());
-        return deleteDict(key);
-    }
-    else if (x[1] == "update") {
-        int key;
-        string value;
-        key = atoi(x[2].c_str());
-        value = x[3];
-        return updateDict(key, value);
-    }
-    else if (x[1] == "concat") {
-        int key1, key2;
-        key1 = atoi(x[2].c_str());
-        key2 = atoi(x[3].c_str());
-        return concatDict(key1, key2);
-    }
-    else if (x[1] == "fetch") {
-        int key;
-        key = atoi(x[2].c_str());
-        return fetchDict(key);
-    }
-    else {
-        return "Invalid query";
-    }
+string parseQueries(vector<string> &tokens) {
+	if (tokens[1] == "insert") {
+		int key;
+		string value;
+		key = atoi(tokens[2].c_str());
+		value = tokens[3];
+		return insertDict(key, value);
+	} else if (tokens[1] == "delete") {
+		int key;
+		key = atoi(tokens[2].c_str());
+		return deleteDict(key);
+	} else if (tokens[1] == "update") {
+		int key;
+		string value;
+		key = atoi(tokens[2].c_str());
+		value = tokens[3];
+		return updateDict(key, value);
+	} else if (tokens[1] == "concat") {
+		int key1, key2;
+		key1 = atoi(tokens[2].c_str());
+		key2 = atoi(tokens[3].c_str());
+		return concatDict(key1, key2);
+	} else if (tokens[1] == "fetch") {
+		int key;
+		key = atoi(tokens[2].c_str());
+		return fetchDict(key);
+	} else {
+		return "Invalid query";
+	}
 }
-void handle_connection(int client_socket_fd)
-{
-    //####################################################
+void handle_connection(int client_socket_fd) {
 
-    int received_num, sent_num;
+	string request;
+	int received_num;
 
-    /* read message from client */
-    int ret_val = 1;
+	tie(request, received_num) =
+		read_string_from_socket(client_socket_fd, buff_sz);
 
-    while (true)
-    {
-        string cmd;
-        tie(cmd, received_num) = read_string_from_socket(client_socket_fd, buff_sz);
-        stringstream temp(cmd);
-        vector<string> x;
-        string word;
-        while (temp >> word) {
-            x.push_back(word);
-        }
-        string output = parseQueries(x);
-        ret_val = received_num;
-        if (ret_val <= 0)
-        {
-            // perror("Error read()");
-            printf("Server could not read msg sent from client\n");
-            goto close_client_socket_ceremony;
-        }
-        cout << "Client sent : " << cmd << endl;
-        // if (cmd == "exit")
-        // {
-        //     cout << "Exit pressed by client" << endl;
-        //     goto close_client_socket_ceremony;
-        // }
-        // string msg_to_send_back = "Ack: " + cmd;
-        pid_t q = gettid();
-        string msg_to_send_back = x[0] + ":" + to_string(q) + ":" + output + "\n"; 
+	if (received_num <= 0) {
+		printf("Server could not read msg sent from client\n");
+		close(client_socket_fd);
+		printf(BRED "Disconnected from client" ANSI_RESET "\n");
+		return;
+	}
 
-        sleep(2); //Artificial wait
-        int sent_to_client = send_string_on_socket(client_socket_fd, msg_to_send_back);
-        // debug(sent_to_client);
-        if (sent_to_client == -1)
-        {
-            perror("Error while writing to client. Seems socket has been closed");
-            goto close_client_socket_ceremony;
-        }
-             goto close_client_socket_ceremony;
-    }
+	// Tokenizing data received from client
+	stringstream request_stream(request);
+	vector<string> tokens;
+	string word;
+	while (request_stream >> word)
+		tokens.push_back(word);
 
-close_client_socket_ceremony:
-    close(client_socket_fd);
-    printf(BRED "Disconnected from client" ANSI_RESET "\n");
+	string output = parseQueries(tokens);
+
+
+	pid_t thread_id = gettid();
+	string client_message =
+		tokens[0] + ":" + to_string(thread_id) + ":" + output + "\n";
+
+	sleep(2); // Artificial wait
+
+	if (send_string_on_socket(client_socket_fd, client_message) == -1) {
+		perror("Error while writing to client");
+	}
+
+	close(client_socket_fd);
+	printf(BRED "Disconnected from client" ANSI_RESET "\n");
 }
-void* threadfunction(void* arg) {
-    while (1) {
-        pthread_mutex_lock(&queue_lock);
-        while (clients.empty()) {
-            pthread_cond_wait(&client_lock, &queue_lock);
-        }
-        int p = clients.front();
-        clients.pop();
-        pthread_mutex_unlock(&queue_lock);
-        handle_connection(p);
-    }
-    return NULL;
+void *threadfunction(void *arg) {
+
+	// Not busy wait, as we use a condition variable inside
+	while (1) {
+
+		pthread_mutex_lock(&queue_lock);
+
+		// Wait till you are signaled and there are clients in the queue
+		while (clients.empty()) pthread_cond_wait(&client_lock, &queue_lock);
+		int socket_fd = clients.front();
+		clients.pop();
+
+		pthread_mutex_unlock(&queue_lock);
+
+		handle_connection(socket_fd);
+	}
+
+	return NULL;
 }
-// Hello
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        cout << "invalid argument count\n";
-        return 0;
-    }
-    pool_size = atoi(argv[1]);
-    thread_pool = (pthread_t *)malloc(pool_size * sizeof(pthread_t));
-    for (int i = 0; i < pool_size; i++) {
-        pthread_create(&thread_pool[i], NULL, threadfunction, NULL);
-    }
-    int i, j, k, t, n;
 
-    int wel_socket_fd, client_socket_fd, port_number;
-    socklen_t clilen;
+int main(int argc, char *argv[]) {
 
-    struct sockaddr_in serv_addr_obj, client_addr_obj;
-    wel_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (wel_socket_fd < 0)
-    {
-        perror("ERROR creating welcoming socket");
-        exit(-1);
-    }
+	// Finding thread pool size
+	if (argc != 2) {
+		cout << "invalid argument count\n";
+		return 0;
+	}
+	pool_size = atoi(argv[1]);
 
-    bzero((char *)&serv_addr_obj, sizeof(serv_addr_obj));
-    port_number = PORT_ARG;
-    serv_addr_obj.sin_family = AF_INET;
-    serv_addr_obj.sin_addr.s_addr = INADDR_ANY;
-    serv_addr_obj.sin_port = htons(port_number); //process specifies port
-    if (bind(wel_socket_fd, (struct sockaddr *)&serv_addr_obj, sizeof(serv_addr_obj)) < 0)
-    {
-        perror("Error on bind on welcome socket: ");
-        exit(-1);
-    }
 
-    listen(wel_socket_fd, MAX_CLIENTS);
-    cout << "Server has started listening on the LISTEN PORT" << endl;
-    clilen = sizeof(client_addr_obj);
+	// Creating the threadpool
+	thread_pool = (pthread_t *)malloc(pool_size * sizeof(pthread_t));
+	for (int i = 0; i < pool_size; i++) {
+		pthread_create(&thread_pool[i], NULL, threadfunction, NULL);
+	}
 
-    while (1)
-    {
-        printf("Waiting for a new client to request for a connection\n");
-        client_socket_fd = accept(wel_socket_fd, (struct sockaddr *)&client_addr_obj, &clilen);
-        if (client_socket_fd < 0)
-        {
-            perror("ERROR while accept() system call occurred in SERVER");
-            exit(-1);
-        }
 
-        printf(BGRN "New client connected from port number %d and IP %s \n" ANSI_RESET, ntohs(client_addr_obj.sin_port), inet_ntoa(client_addr_obj.sin_addr));
-        
-        pthread_mutex_lock(&queue_lock);
-        clients.push(client_socket_fd);
-        pthread_cond_signal(&client_lock);
-        pthread_mutex_unlock(&queue_lock);
-    }
+	struct sockaddr_in serv_addr_obj, client_addr_obj;
+	int wel_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (wel_socket_fd < 0) {
+		perror("ERROR creating welcoming socket");
+		exit(-1);
+	}
 
-    close(wel_socket_fd);
-    return 0;
+	bzero((char *)&serv_addr_obj, sizeof(serv_addr_obj));
+
+	serv_addr_obj.sin_family = AF_INET;
+	serv_addr_obj.sin_addr.s_addr = INADDR_ANY;
+	serv_addr_obj.sin_port = htons(SERVER_PORT); // process specifies port
+	if (bind(wel_socket_fd, (struct sockaddr *)&serv_addr_obj,
+			 sizeof(serv_addr_obj)) < 0) {
+		perror("Error on bind on welcome socket: ");
+		exit(-1);
+	}
+
+	listen(wel_socket_fd, MAX_CLIENTS);
+
+	socklen_t clilen = sizeof(client_addr_obj);
+
+	while (1) {
+
+		int client_socket_fd =
+			accept(wel_socket_fd, (struct sockaddr *)&client_addr_obj, &clilen);
+		if (client_socket_fd < 0) {
+			perror("ERROR while accept() system call occurred in SERVER");
+			exit(-1);
+		}
+
+		printf(
+			BGRN
+			"New client connected from port number %d and IP %s \n" ANSI_RESET,
+			ntohs(client_addr_obj.sin_port),
+			inet_ntoa(client_addr_obj.sin_addr));
+
+
+		// Adding the client fd to the client queue, which will eventually be picked up by the worker threads
+		pthread_mutex_lock(&queue_lock);
+
+		clients.push(client_socket_fd);
+		pthread_cond_signal(&client_lock);
+
+		pthread_mutex_unlock(&queue_lock);
+	}
+
+	close(wel_socket_fd);
+	return 0;
 }
